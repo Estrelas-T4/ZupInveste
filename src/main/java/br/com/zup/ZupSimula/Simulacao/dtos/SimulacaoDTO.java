d51d58cce72aefb395d1b7c3b6494c091e5ef1b5
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-05 10:16:42 -0300

    Criar DTO de entrada para Simulacao

---
+++ src/main/java/br/com/zup/ZupSimula/Simulacao/dtos/SimulacaoDTO.java
@@ -0,0 +1,62 @@
+package br.com.zup.ZupSimula.Simulacao.dtos;
+
+//lombok cria gets e sets automaticamente.
+public class SimulacaoDTO {
+    private String email;
+    private String nome;
+    private String cpf;
+    private double valorInvestimento;
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public SimulacaoDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorInvestimento() {
+        return valorInvestimento;
+    }
+
+    public void setValorInvestimento(double valorInvestimento) {
+        this.valorInvestimento = valorInvestimento;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
