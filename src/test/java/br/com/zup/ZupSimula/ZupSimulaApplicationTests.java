a138b03c9d16e39be550e3f975af4c8980e4b902
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-05 10:09:22 -0300

    Primeiro commit

---
+++ src/test/java/br/com/zup/ZupSimula/ZupSimulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupSimula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupSimulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
