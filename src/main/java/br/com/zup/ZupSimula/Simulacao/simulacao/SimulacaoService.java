2eb76e89a7010b0b471aed0671468c08c04f15d8
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-05 10:24:27 -0300

    Criar servico para simulacao

---
+++ src/main/java/br/com/zup/ZupSimula/Simulacao/simulacao/SimulacaoService.java
@@ -0,0 +1,14 @@
+package br.com.zup.ZupSimula.Simulacao.simulacao;
+
+import br.com.zup.ZupSimula.Simulacao.dtos.SimulacaoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SimulacaoService {
+    private List<SimulacaoDTO> simulacaoDTOS = new ArrayList<>();
+
+
+}
